161c4d01d8efec70cad65dce8c44e177f663f0c9
Al Maz <alexmaznitskiy@gmail.com>
2022-04-05 14:59:39 +0300

    added 5.11

---
+++ Lab5/Task11/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let answer_to = function
+    | "f#" | "prolog" -> "Пoдлизa!"
+    | "python" -> "Худший язык"
+    | "c#" -> "Лучший язык"
+    | "c++" -> "База"
+    | _ -> "Ты такие вещи не говори"
+
+[<EntryPoint>]
+let main argv =
+    printfn "Какoй твoй любимый язык?"
+    let language = Console.ReadLine().ToLower()
+    Console.WriteLine(answer_to language)
+    0
