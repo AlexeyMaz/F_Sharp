7ddcac5eaf5d3997ac7ef07236031ca9d1be5406
Al Maz <alexmaznitskiy@gmail.com>
2022-04-05 16:48:32 +0300

    added 5.12

---
+++ Lab5/Task12/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+let answer_to = function
+    | "f#" | "prolog" -> "Пoдлизa!"
+    | "python" -> "Худший язык"
+    | "c#" -> "Лучший язык"
+    | "c++" -> "База"
+    | _ -> "Ты такие вещи не говори"
+
+[<EntryPoint>]
+let main argv =
+   System.Console.WriteLine("Какoй твoй любимый язык?")
+   //Случай суперпозиции
+   (Console.ReadLine>>answer_to>>Console.WriteLine)()
+
+   //----------------------------------------------------------------------//
+
+   System.Console.WriteLine("Какoй твoй любимый язык?")
+  // Случай каррирования
+   let F y x z =z(x(y()))
+   F Console.ReadLine answer_to Console.WriteLine
+   0
