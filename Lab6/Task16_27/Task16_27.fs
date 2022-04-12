3f19da8cd85b9b6f630427231a140ed489dfb79d
Al Maz <alexmaznitskiy@gmail.com>
2022-04-12 11:59:57 +0300

    added 6.16 (27)

---
+++ Lab6/Task16_27/Task16_27.fs
@@ -0,0 +1,28 @@
+﻿open System
+
+let rec readList n = 
+    if n = 0 then []
+    else
+        let Head = Console.ReadLine() |> Convert.ToInt32
+        let Tail = readList (n-1)
+        Head::Tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        Console.WriteLine($"{head}")
+        writeList tail
+
+let shift_left (list: 'a list) =
+   if list.Length <= 1 then list
+   else
+       list.Tail @ [list.Head]
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите кол-во элементов и список:")
+    let list = readList (Console.ReadLine() |> Convert.ToInt32)
+    let new_list = shift_left list
+    Console.WriteLine("Сдвинутый список:")
+    writeList new_list
+    0
