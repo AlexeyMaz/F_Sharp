ac12dc907ae2609dab68ddd9431ad1b137251d9a
Al Maz <alexmaznitskiy@gmail.com>
2022-04-22 15:39:43 +0300

    added 7.12 (16)

---
+++ Lab7/Task12_16/Task12_16.fs
@@ -0,0 +1,36 @@
+﻿open System
+
+let rec readList n = 
+    if n = 0 then []
+    else
+        let Head = Console.ReadLine() |> Convert.ToInt32
+        let Tail = readList (n-1)
+        Head::Tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        Console.WriteLine($"{head}")
+        writeList tail
+
+let ElemsBetweenMax list = 
+   let max = List.max list
+   
+   let max1_index = List.findIndex (fun (index, x) -> x = max) (List.indexed list)
+   let max2_index = List.tryFindIndex (fun (index, x) -> index <> max1_index && x = max) (List.indexed list)
+   
+   if max2_index = None then
+       []
+   else
+       let between_indexed = List.filter (fun x -> fst x > max1_index && fst x < Option.get max2_index) (List.indexed list)
+       List.map (fun x -> snd x) between_indexed
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите кол-во элементов и список:")
+    let list = readList (Console.ReadLine() |> Convert.ToInt32)
+
+    let result = ElemsBetweenMax list
+    Console.WriteLine("Элементы между первым и вторым максимумами:")
+    writeList result
+    0
--- Lab7/Task12_16/obj/Debug/netcoreapp3.1/Task12_16.AssemblyInfo.fs
+++ Lab7/Task12_16/obj/Debug/netcoreapp3.1/Task12_16.AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System
 open System.Reflection
 
 
-[<assembly: System.Reflection.AssemblyCompanyAttribute("Lab7")>]
+[<assembly: System.Reflection.AssemblyCompanyAttribute("Task12_16")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
 [<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
-[<assembly: System.Reflection.AssemblyProductAttribute("Lab7")>]
-[<assembly: System.Reflection.AssemblyTitleAttribute("Lab7")>]
+[<assembly: System.Reflection.AssemblyProductAttribute("Task12_16")>]
+[<assembly: System.Reflection.AssemblyTitleAttribute("Task12_16")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
 do()
