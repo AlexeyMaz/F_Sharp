c592c718cedae07da265b5cb789864602dc08b95
Al Maz <alexmaznitskiy@gmail.com>
2022-04-22 21:03:14 +0300

    added 7.14 (36)

---
+++ Lab7/Task14_36/Task14_36.fs
@@ -0,0 +1,28 @@
+﻿open System
+
+let rec readList n = 
+    if n = 0 then []
+    else
+        let Head = Console.ReadLine() |> Convert.ToInt32
+        let Tail = readList (n-1)
+        Head::Tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        Console.WriteLine($"{head}")
+        writeList tail
+
+let GetMaxOdd list = 
+    let sorted = List.sortDescending list
+    List.tryFind (fun x -> x % 2 = 1) sorted
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите кол-во элементов и список:")
+    let list = readList (Console.ReadLine() |> Convert.ToInt32)
+
+    let max_odd = GetMaxOdd list
+    if Option.isNone max_odd then Console.WriteLine("В списке нет нечетных элементов")
+    else Console.WriteLine($"Максимальный нечетный элемент: {Option.get max_odd}")
+    0
