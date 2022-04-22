410f07036e1eb93c4bdbd0814efdf27a7be89970
Al Maz <alexmaznitskiy@gmail.com>
2022-04-22 21:44:32 +0300

    added 7.15 (46)

---
+++ Lab7/Task15_46/Task15_46.fs
@@ -0,0 +1,26 @@
+﻿open System
+
+let rec readList n = 
+    if n = 0 then []
+    else
+        let Head = Console.ReadLine() |> Convert.ToInt32
+        let Tail = readList (n-1)
+        Head::Tail
+
+let rec writeList = function
+    | [] -> ()
+    | head::tail -> 
+        Console.WriteLine($"{head}")
+        writeList tail
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Введите кол-во элементов и список:")
+    let list = readList (Console.ReadLine() |> Convert.ToInt32)
+
+    Console.WriteLine("Все положительные элементы:")
+    List.filter (fun x -> x > 0) list |> writeList
+
+    Console.WriteLine("Все отрицательные элементы:")
+    List.filter (fun x -> x < 0) list |> writeList
+    0
