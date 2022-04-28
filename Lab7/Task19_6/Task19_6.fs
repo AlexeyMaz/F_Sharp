0d82f9de8fcaa073adf74e8ed181e8a0d9ae4b6c
Al Maz <alexmaznitskiy@gmail.com>
2022-04-28 20:03:25 +0300

    added 19.6 (subtask 12)

--- Lab7/Task19_6/Task19_6.fs
+++ Lab7/Task19_6/Task19_6.fs
@@ -11,6 +11,12 @@ let Subtask6 s =
         let transposition = [0] @ (List.sortBy(fun _ -> rnd.Next(1, length-2)) order) @ [length - 1]
         String.init length (fun index -> s.[transposition.[index]].ToString())
 
+//Расположить все цифры в начале строки, а буквы - в конце
+let arrange s =
+    let digits = String.filter (fun elem -> Char.IsDigit elem) s
+    let letters = String.filter (fun elem -> Char.IsLetter elem) s
+    String.concat "" [digits; letters]
+
 [<EntryPoint>]
 let main argv =
 
