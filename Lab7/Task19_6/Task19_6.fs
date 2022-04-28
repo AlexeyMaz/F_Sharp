e0ea3a0204b972538f8c0e9d8e892b905d5224a1
Al Maz <alexmaznitskiy@gmail.com>
2022-04-28 20:16:31 +0300

    added full 7.19

--- Lab7/Task19_6/Task19_6.fs
+++ Lab7/Task19_6/Task19_6.fs
@@ -12,12 +12,22 @@ let Subtask6 s =
         String.init length (fun index -> s.[transposition.[index]].ToString())
 
 //Расположить все цифры в начале строки, а буквы - в конце
-let arrange s =
+let Subtask12 s =
     let digits = String.filter (fun elem -> Char.IsDigit elem) s
     let letters = String.filter (fun elem -> Char.IsLetter elem) s
     String.concat "" [digits; letters]
 
+let choose = function
+| 1 -> Subtask6
+| _ -> Subtask12
+
 [<EntryPoint>]
 let main argv =
+   printfn "Введите строку: "
+   let str = Console.ReadLine()
+   printfn "Какую задачу решить?"
+   printfn "1. Перемешать символы в слове, кроме первого и последнего"
+   printfn "2. Расположить все цифры в начале строки, а буквы - в конце"
 
+   Console.ReadLine() |> Int32.Parse |> choose <| str |> printfn "Результат: %s" 
    0
