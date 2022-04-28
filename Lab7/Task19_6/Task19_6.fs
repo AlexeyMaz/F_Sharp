635b6f2e4b1eb204a8cb88632916ae79ce9c766d
Al Maz <alexmaznitskiy@gmail.com>
2022-04-28 19:26:07 +0300

    added 19.6 (subtask 6)

---
+++ Lab7/Task19_6/Task19_6.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+//Перемешать символы в слове, кроме первого и последнего
+let Subtask6 s =
+    let length = String.length s
+    if length <= 3 then
+        s
+    else
+        let rnd = System.Random()
+        let order = [1 .. length-2]
+        let transposition = [0] @ (List.sortBy(fun _ -> rnd.Next(1, length-2)) order) @ [length - 1]
+        String.init length (fun index -> s.[transposition.[index]].ToString())
+
+[<EntryPoint>]
+let main argv =
+
+   0
