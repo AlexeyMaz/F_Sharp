8d95bc2d48949fd7085fdbcfd565f71b79c41c75
Al Maz <alexmaznitskiy@gmail.com>
2022-05-04 16:57:37 +0300

    added 9.6

--- Lab9/Task6/obj/Debug/net472/Task6.AssemblyInfo.fs
+++ Lab9/Task6/obj/Debug/net472/Task6.AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System
 open System.Reflection
 
 
-[<assembly: System.Reflection.AssemblyCompanyAttribute("Lab9")>]
+[<assembly: System.Reflection.AssemblyCompanyAttribute("Task6")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
 [<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
-[<assembly: System.Reflection.AssemblyProductAttribute("Lab9")>]
-[<assembly: System.Reflection.AssemblyTitleAttribute("Lab9")>]
+[<assembly: System.Reflection.AssemblyProductAttribute("Task6")>]
+[<assembly: System.Reflection.AssemblyTitleAttribute("Task6")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
 do()
