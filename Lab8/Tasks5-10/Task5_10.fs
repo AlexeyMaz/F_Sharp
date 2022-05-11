bcec7f9b184434a0b1b261f7654a8442b36a91fb
Al Maz <alexmaznitskiy@gmail.com>
2022-05-11 23:46:09 +0300

    added 8.6

--- Lab8/Tasks5-10/Task5_10.fs
+++ Lab8/Tasks5-10/Task5_10.fs
@@ -1,16 +1,23 @@
 ﻿open System
 
-type DriversLicense(first_name: string, last_name: string, series: int, number: int, issDate: DateTime, expDate: DateTime) =
-    member this.first_name = first_name
-    member this.last_name = last_name
+type DriversLicense(surname: string, firstname: string, patronymic : string, series: int, number: int, issDate: DateTime, expDate: DateTime, categories: string) =
+    member this.firstname = firstname
+    member this.surname = surname
+    member this.patronymic = patronymic
     member this.series = series
     member this.number = number
     member this.issDate = issDate
     member this.expDate = expDate
+    member this.categories = categories
 
     member this.isValid() = DateTime.Now < expDate
 
+    override this.ToString() = $"Водительское удостоверение {{Фамилия: {this.surname}, Имя: {this.firstname}, Отчество: {this.patronymic}, " + 
+    $"Серия: {this.series}, Выдано: {this.issDate.ToShortDateString()}, Истекает: {this.expDate.ToShortDateString()}, Категории: {this.categories}}}"
+
 [<EntryPoint>]
 let main argv =
+   let lic1 = DriversLicense("Левин", "Валентин", "Александрович", 9966, 471030, DateTime.Parse "12.12.2020", DateTime.Parse "12.12.2030", "B, B1, M")
+   Console.WriteLine(lic1)
 
-    0
+   0
