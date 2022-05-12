b48d180bd2c4b849d9251baa2ddd757bb45b103f
Al Maz <alexmaznitskiy@gmail.com>
2022-05-12 17:09:18 +0300

    added 8.8

--- Lab8/Task5-10/Task5_10.fs
+++ Lab8/Task5-10/Task5_10.fs
@@ -1,4 +1,5 @@
 ﻿open System
+open System.Text.RegularExpressions
 
 type DriversLicense(surname: string, firstname: string, patronymic : string, series: int, number: int, issDate: DateTime, expDate: DateTime, categories: string) =
     member this.firstname = firstname
@@ -26,12 +27,50 @@ type DriversLicense(surname: string, firstname: string, patronymic : string, ser
        | :? DriversLicense as lic2 -> (this.series = lic2.series) && (this.number = lic2.number)
        | _ -> false
 
-     override this.ToString() = $"Водительское удостоверение {{Фамилия: {this.surname}, Имя: {this.firstname}, Отчество: {this.patronymic}, " + 
-    $"Серия: {this.series}, Выдано: {this.issDate.ToShortDateString()}, Истекает: {this.expDate.ToShortDateString()}, Категории: {this.categories}}}"
+    override this.ToString() = $"Водительское удостоверение {{Фамилия: {this.surname}, Имя: {this.firstname}, Отчество: {this.patronymic}, Серия: {this.series}, Номер: {this.number}, Выдано: {this.issDate.ToShortDateString()}, Истекает: {this.expDate.ToShortDateString()}, Категории: {this.categories}}}"
+
+//Кидает ошибку если строка не удовлетворяет регулярке
+let assertRegex str regex =
+    let r = Regex(regex)
+    if not (r.IsMatch str) then
+        invalidArg str $"Строка не удовлетворяет формату: {regex}"
+
+//Ввод поля в соотв. с регуляркой (пока не будет введено верно)
+let rec inputField prompt regex =
+    printf $"{prompt}: "
+    let input = Console.ReadLine()
+    try
+        assertRegex input regex
+        input
+    with
+        | :? System.ArgumentException as e ->
+            printfn "Ошибка: %s" e.Message
+            inputField prompt regex
+        | e ->
+            printfn "Непредвиденное исключение: %s" e.Message
+            reraise()
+
+let inputLicense() =
+    printfn "--- Создание водительского удостоверения ---"
+
+    let lastname = inputField "Фамилия" "^[a-zA-Zа-яА-Я]*$"
+    let firstname = inputField "Имя" "^[a-zA-Zа-яА-Я]*$"
+    let patronymic = inputField "Отчество" "^[a-zA-Zа-яА-Я]*$"
+    let series = inputField "Серия" "^\d{4}$" |> Int32.Parse
+    let number = inputField "Номер" "^\d{6}$" |> Int32.Parse
+    let issDate = inputField "Дата выдачи" "^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$" |> DateTime.Parse
+    let expDate = inputField "Дата окончания" "^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$" |> DateTime.Parse
+    let categories = inputField "Категории" "^\s*((A1?|B[1E]?|C1?E?|D1?E?|M|Tm|Tb)(\,\s)*)+\s*$"
+
+    DriversLicense(lastname, firstname, patronymic, series, number, issDate, expDate, categories)
 
 [<EntryPoint>]
 let main argv =
    let lic1 = DriversLicense("Левин", "Валентин", "Александрович", 9966, 471030, DateTime.Parse "17.09.2020", DateTime.Parse "17.09.2030", "B, B1, M")
    Console.WriteLine(lic1)
+   Console.WriteLine()
 
+   let lic2 = inputLicense()
+   Console.WriteLine()
+   Console.WriteLine(lic2)
    0
